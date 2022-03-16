faac48e6768ecfbfffd520a2ea4e12035e9e6eec
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-16 17:36:32 +1300

    [feat]: Updated layout for UI and routing.
    Updated layout for ionic to run and render UI properly and with the routing layout done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
+import { Route } from 'react-router-dom';
+
+import '@ionic/react/css/core.css';
+
+// /* Basic CSS for apps built with Ionic */
+// import '@ionic/react/css/normalize.css';
+// import '@ionic/react/css/structure.css';
+// import '@ionic/react/css/typography.css';
+
+// /* Optional CSS utils that can be commented out */
+// import '@ionic/react/css/padding.css';
+// import '@ionic/react/css/float-elements.css';
+// import '@ionic/react/css/text-alignment.css';
+// import '@ionic/react/css/text-transformation.css';
+// import '@ionic/react/css/flex-utils.css';
+// import '@ionic/react/css/display.css';
+
+// Required to get the app working!!!
+setupIonicReact()
+
 function App() {
   return (
-    <div>
-      <header>
-        Header
-      </header>
-      Food app
-    </div>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          {/* <Route path="/" exact component={Home} />
+          <Route path="/otherPage" exact component={OtherPage} /> */}
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
   );
 }
 
