2bdb11520c09e41dd89fec295d048e709a9d6af6
henrym25 <henry3399@hotmail.co.nz>
2022-03-19 09:44:31 +1300

    Added email validation to user schema

--- backend/src/models/User/UserSchema.js
+++ backend/src/models/User/UserSchema.js
@@ -7,7 +7,17 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   displayName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: (email) => {
+      //Validates the email by checking if it follows either 'test@test.com' or 'test@test.co.nz' format
+      return (
+        /[a-zA-z]+@[a-zA-z]+\.[a-zA-z]+\.[a-zA-z]+/.test(email) ||
+        /[a-zA-z]+@[a-zA-z]+\.[a-zA-z]+/.test(email)
+      );
+    },
+  },
   password: { type: String, required: true },
   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   favourites: [{ type: Schema.Types.ObjectId, ref: "Post" }],
