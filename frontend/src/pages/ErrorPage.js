f4bf33ab1f36bb577b2b788f81746b9056d35ec5
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-22 18:47:39 +1300

    [feat]: 404 Error Message.
    404 Error Message page on incorrect link implemented.

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,10 +1,12 @@
 import { IonPage } from "@ionic/react"
+import { Error404 } from "../components/Error404"
 import { Navbar } from "../components/Navbar"
 
 export const ErrorPage = () => {
     return (
         <IonPage>
             <Navbar />
+            <Error404 />
         </IonPage>
     )
 }
