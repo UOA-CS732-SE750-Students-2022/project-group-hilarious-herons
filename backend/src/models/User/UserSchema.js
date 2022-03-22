d8fc36c6244044d78908629449ef72f196a18ddf
henrym25 <henry3399@hotmail.co.nz>
2022-03-23 09:11:40 +1300

    Added first name and last name to user schema

--- backend/src/models/User/UserSchema.js
+++ backend/src/models/User/UserSchema.js
@@ -7,6 +7,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   displayName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: {
     type: String,
     required: true,
