61770a9c6f0bb658d7e65600f8144f7e2ef5f401
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-23 14:48:57 +1300

    Setting Themes for Frontend (#28)
    * [dist]: Added ionic and react router dependencies.
    
    Added ionic and react router dependencies. For UI and routing.
    
    * [feat]: Updated layout for UI and routing.
    
    Updated layout for ionic to run and render UI properly and with the routing layout done.
    
    * [feat]: Resources.
    
    Made resources folder and added banner image.
    
    * [feat]: Started homepage and header.
    
    Implemented basic homepage setup and added the header for it.
    
    * [feat]: Basic navbar and browse page.
    
    Implemented basic navbar with the colour scheme on browse page.
    
    * [refactor]: Browse to Food Details.
    
    Changed Browse page to Food Details page.
    
    * [feat]: Added new pages.
    
    Added new pages to be routed to.
    
    * [feat]: Not Found.
    
    Not Found page routing and incorrect link redirection to it implemented.
    
    * [style]: Added ionic styles.
    
    Added ionic styles to index.css.
    
    * [style]: Updated themes.
    
    Updated themes to account for different texts and backgrounds - dark and light.
    
    * [fix]: Search bar text colour.
    
    Search bar text colour needed to be specified explicitly.
    
    * [style]: Icon colour.
    
    Set Ion Icon colour to primary.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,8 @@ import { IonHeader, IonSearchbar, IonTitle } from "@ionic/react"
 export const Navbar = () => {
     return (
         <IonHeader style={{ backgroundColor: "#FF9F1C", display: "flex" }}>
-            <IonTitle style={{ color: "white" }}><h2>FUNTER</h2></IonTitle>
-            <IonSearchbar style={{ width: '50%', paddingRight: "3%" }} />
+            <IonTitle color="ionHeaderText"><h2>FUNTER</h2></IonTitle>
+            <IonSearchbar style={{ width: '50%', paddingRight: "3%", color: "#727272" }} />
         </IonHeader>
     )
 }
