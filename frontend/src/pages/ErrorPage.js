05cb6cb5095de89072cedd9e0dc44820d87c90ab
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-23 17:10:20 +1300

    Error page (#32)
    * [dist]: Added ionic and react router dependencies.
    
    Added ionic and react router dependencies. For UI and routing.
    
    * [feat]: Updated layout for UI and routing.
    
    Updated layout for ionic to run and render UI properly and with the routing layout done.
    
    * [feat]: Resources.
    
    Made resources folder and added banner image.
    
    * [feat]: Started homepage and header.
    
    Implemented basic homepage setup and added the header for it.
    
    * [feat]: Basic navbar and browse page.
    
    Implemented basic navbar with the colour scheme on browse page.
    
    * [refactor]: Browse to Food Details.
    
    Changed Browse page to Food Details page.
    
    * [feat]: Added new pages.
    
    Added new pages to be routed to.
    
    * [feat]: Not Found.
    
    Not Found page routing and incorrect link redirection to it implemented.
    
    * [style]: Added ionic styles.
    
    Added ionic styles to index.css.
    
    * [style]: Updated themes.
    
    Updated themes to account for different texts and backgrounds - dark and light.
    
    * [feat]: 404 Error Message.
    
    404 Error Message page on incorrect link implemented.
    
    * [fix]: Search bar text colour.
    
    Search bar text colour needed to be specified explicitly.
    
    * [style]: Icon colour.
    
    Set Ion Icon colour to primary.
    
    * [feat]: Navigation to home page.
    
    Navigation to home page implemented.

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,10 +1,13 @@
 import { IonPage } from "@ionic/react";
 import { ActionHeader } from "../components/ActionHeader";
+import { Error404 } from "../components/Error404"
 
 export const ErrorPage = () => {
   return (
     <IonPage>
-      <ActionHeader></ActionHeader>
+      <ActionHeader>
+            <Error404 />
+      </ActionHeader>
     </IonPage>
   );
 };
