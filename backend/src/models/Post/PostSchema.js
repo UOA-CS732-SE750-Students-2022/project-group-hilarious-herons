f60e475ad66cdf236e425b745e90c9416e7dd79d
hman25 <67317842+henrym25@users.noreply.github.com>
2022-03-25 16:35:00 +1300

    Added additional fields to PostSchema and RestaurantSchema (#40)

--- backend/src/models/Post/PostSchema.js
+++ backend/src/models/Post/PostSchema.js
@@ -6,13 +6,13 @@ const postSchema = new Schema(
   {
     foodName: String,
     bodyText: { type: String, required: true },
-    tags: [{ type: String }], //Add at least 1 tag validation requirement
+    tags: [{ type: String, required: true }], //Add at least 1 tag validation requirement
     dietryRequirements: [{ type: String }],
     restaurant: { type: Schema.Types.ObjectId, ref: "Restaurant" },
     numberOfLikes: Number,
-    // image : TODO : Figure out how to include image in schema
     rating: Number,
     numberOfReviews: Number,
+    imageURLs: [{ type: String }],
   },
   {
     timestamp: {},
