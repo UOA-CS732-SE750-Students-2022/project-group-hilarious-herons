e9964b23c335d1cd882cb430629dd840331df328
Yujia Wu <49899381+ywu666@users.noreply.github.com>
2022-03-25 18:34:12 +0800

    Food Card Component (#30)
    * Delete the logos and add food card components
    
    * Move the search bar to the center of the headers.
    
    * Add image into the foodcard
    
    * Finalise the food card component
    
    * Delete unused the import.
    
    * Add background colour to rate and distance make is become more obvious on the image.
    
    * Enhance: using chip to show the distance and rate. But the colour theme need to change.
    
    * Style the Ionchip
    
    * Align the tags to the left
    
    * Style the code
    
    * Adjust the likes in one line
    
    * Move the style to css file

---
+++ frontend/src/components/HomeHeader.js
@@ -0,0 +1,14 @@
+import { IonHeader, IonSearchbar, IonTitle } from "@ionic/react"
+import banner from './resources/banner.png'
+
+export const HomeHeader = () => {
+    return (
+        <IonHeader style={{ backgroundImage: `url(${banner})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPositon: "center", height: '30%' }}>
+            <div className='home-header-content'>
+                <IonTitle style={{ color: "white" }}><h2>FUNTER</h2></IonTitle>
+                <IonSearchbar style={{ width: '50%', float: "right", paddingRight: "3%" }} />
+                <IonTitle style={{ color: "white" }}><h1>Something about the app</h1></IonTitle>
+            </div>
+        </IonHeader>
+    )
+}
