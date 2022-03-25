a04724b24f0bbb1c3f1b4b23496b65ee14eb2555
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-25 22:09:47 +1300

    Style colour bug fix (#34)
    * [dist]: Added ionic and react router dependencies.
    
    Added ionic and react router dependencies. For UI and routing.
    
    * [feat]: Updated layout for UI and routing.
    
    Updated layout for ionic to run and render UI properly and with the routing layout done.
    
    * [feat]: Resources.
    
    Made resources folder and added banner image.
    
    * [feat]: Started homepage and header.
    
    Implemented basic homepage setup and added the header for it.
    
    * [feat]: Basic navbar and browse page.
    
    Implemented basic navbar with the colour scheme on browse page.
    
    * [refactor]: Browse to Food Details.
    
    Changed Browse page to Food Details page.
    
    * [feat]: Added new pages.
    
    Added new pages to be routed to.
    
    * [feat]: Not Found.
    
    Not Found page routing and incorrect link redirection to it implemented.
    
    * [style]: Added ionic styles.
    
    Added ionic styles to index.css.
    
    * [style]: Updated themes.
    
    Updated themes to account for different texts and backgrounds - dark and light.
    
    * [feat]: 404 Error Message.
    
    404 Error Message page on incorrect link implemented.
    
    * [fix]: Search bar text colour.
    
    Search bar text colour needed to be specified explicitly.
    
    * [style]: Icon colour.
    
    Set Ion Icon colour to primary.
    
    * [feat]: Navigation to home page.
    
    Navigation to home page implemented.
    
    * [style]: Fixed colour choices.
    
    Fixed colour choices for ionHeaderText.
    
    * [style]: Removed button shadows.
    
    Removed button shadows.
    
    * [fix]: Corrected colour used.
    
    Corrected colours used by Ion components.
    
    * [fix]: Mode and font weight.
    
    Button font weight set to bold.
    Removed ios mode from login button.

--- frontend/src/components/ActionHeader.js
+++ frontend/src/components/ActionHeader.js
@@ -62,15 +62,14 @@ export const ActionHeader = ({ banner, children }) => {
             class="searchBar"
             mode="ios"
             style={{ padding: "0" }}
-            color="light"
+            color="ionHeaderText"
             padding="0"
           />
           <IonButton
             slot="end"
             shape="round"
-            color="light"
+            color="ionHeaderText"
             style={{ padding: "0 50px" }}
-            mode="ios"
           >
             Login
           </IonButton>
