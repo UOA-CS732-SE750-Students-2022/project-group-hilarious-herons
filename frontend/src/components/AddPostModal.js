d8d2f24db44781a1957093e244ea1186eba48467
Yujia Wu <ywu660@aucklanduni.ac.nz>
2022-03-30 18:49:28 +0800

    Implement the location and experience.

--- frontend/src/components/AddPostModal.js
+++ frontend/src/components/AddPostModal.js
@@ -1,9 +1,69 @@
-import { IonModal, IonContent, IonButton } from '@ionic/react';
+import { 
+  IonModal, 
+  IonContent, 
+  IonButton, 
+  IonButtons,
+  IonIcon,
+  IonTitle, 
+  IonToolbar,
+  IonSearchbar, 
+  IonItem,
+  IonTextarea,
+  
+} from '@ionic/react';
+import { closeOutline, locationOutline} from 'ionicons/icons';
+import { useState } from "react";
+import "./AddPostModal.css";
 
 export const AddPostModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState('true')
+  const [searchLocationText, setSearchLocationText] = useState('');
+  const [experienceText, setExperienceText] = useState('');
+
     return (
-        <IonModal trigger='addPost'>
-        <IonContent>Modal Content</IonContent>
+        <IonModal trigger='addPost' 
+                  isOpen={isModalOpen}
+                  swipeToClose={true} 
+                  mode='ios'>
+        <IonContent>
+          <IonToolbar>
+            <IonTitle color='primary'>Add a Review</IonTitle>
+            <IonButtons slot='primary'>
+              <IonButton color='light' 
+                         onClick={ () => setIsModalOpen(false)}>
+                <IonIcon icon={closeOutline} />
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+
+          <IonToolbar mode='md' className='location'>
+            <IonTitle size='small'>Restaurant Location</IonTitle>
+            <IonSearchbar
+                value={ searchLocationText } 
+                class="location-bar"
+                mode="ios"
+                placeholder='Serach'
+                searchIcon= {locationOutline}
+                showClearButton='never'
+                style={{ padding: '0 1em'}}
+                onIonChange={(e) => {
+                  setSearchLocationText(e.detail.value)
+                }}/> 
+
+              <IonTextarea 
+                placeholder="Share your experience with others ~" 
+                value={experienceText} 
+                rows={6} 
+                enterkeyhint='enter'
+                spellCheck={ true }
+                onIonChange={e => setExperienceText(e.detail.value)}
+                style={{       
+                  border: "1px solid #ccc"
+                }}>
+              </IonTextarea>
+          </IonToolbar>
+
+        </IonContent>
       </IonModal>
     );
 }
