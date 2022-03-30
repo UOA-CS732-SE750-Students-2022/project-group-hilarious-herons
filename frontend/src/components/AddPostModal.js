45313950a7494078a7b559799cdf9d6c2ba5c022
Yujia Wu <ywu660@aucklanduni.ac.nz>
2022-03-30 12:38:33 +0800

    Initialise the add post modal

---
+++ frontend/src/components/AddPostModal.js
@@ -0,0 +1,9 @@
+import { IonModal, IonContent, IonButton } from '@ionic/react';
+
+export const AddPostModal = () => {
+    return (
+        <IonModal trigger='addPost'>
+        <IonContent>Modal Content</IonContent>
+      </IonModal>
+    );
+}
