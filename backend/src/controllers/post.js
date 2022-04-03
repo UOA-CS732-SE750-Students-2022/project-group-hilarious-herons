afa4cc91098a14dcea99192ab042ec7391cd4b17
Kirsty Gong <kirstygong23@gmail.com>
2022-04-03 21:47:11 +1200

    like and unlike api

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -1,4 +1,4 @@
-const { retrievePost, createPost } = require("../models/Post/posts-dao");
+const { retrievePost, updatePost } = require("../models/Post/posts-dao");
 const mongoose = require("mongoose");
 
 exports.getPost = async (req, res) => {
@@ -11,6 +11,68 @@ exports.getPost = async (req, res) => {
     }
 
     res.send(post);
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      info: e.message,
+    });
+  }
+};
+
+exports.likePost = async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const objectId = mongoose.Types.ObjectId(req.body.id);
+    const post = await retrievePost(objectId);
+
+    if (post === undefined || post === null || post.length === 0) {
+      res.status(404);
+    }
+
+    console.log(post);
+    post.numberOfLikes = post.numberOfLikes + 1;
+    console.log(post);
+
+    updatePost(post);
+
+    res.status(200).json({
+      success: true,
+      id: post.id,
+      currentLikes: post.numberOfLikes,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      info: e.message,
+    });
+  }
+};
+
+exports.unlikePost = async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const objectId = mongoose.Types.ObjectId(req.body.id);
+    const post = await retrievePost(objectId);
+
+    if (post === undefined || post === null || post.length === 0) {
+      res.status(404);
+    }
+
+    console.log(post);
+    if (post.numberOfLikes > 0) {
+      post.numberOfLikes = post.numberOfLikes - 1;
+    }
+    console.log(post);
+
+    updatePost(post);
+
+    res.status(200).json({
+      success: true,
+      id: post.id,
+      currentLikes: post.numberOfLikes,
+    });
   } catch (e) {
     res.status(500).json({
       success: false,
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getPost } = require("../controllers/post");
+const { getPost, likePost, unlikePost } = require("../controllers/post");
 
 const router = express.Router();
 
 router.route("/post/:id").get(getPost);
+router.route("/post/like-post").post(likePost);
+router.route("/post/unlike-post").post(unlikePost);
 
 module.exports = router;
