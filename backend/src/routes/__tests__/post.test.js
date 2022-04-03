2ac5f0dea2a58e65d5158332be72c83127c0fc51
Kirsty Gong <kirstygong23@gmail.com>
2022-04-03 18:45:22 +1200

    fix post.test.js

--- backend/src/routes/__tests__/post.test.js
+++ backend/src/routes/__tests__/post.test.js
@@ -1,16 +1,27 @@
 const request = require("supertest");
 const express = require("express");
 const routes = require("../index");
-const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
-
-let mongod;
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use("/", routes);
 
-app.listen(80, () => console.log(`App server listening on port ${80}!`));
+mongoose
+  .connect(
+    `mongodb+srv://db-user:${process.env.MONGO_PASSWORD}@cluster0.vprvj.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then((result) => {
+    console.log("MongoDB connection successful");
+  })
+  .then(() => {
+    app.listen(80, () => console.log(`App server listening on port ${80}!`));
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed : ", error.message);
+  });
 
 describe("GET /post/:id", () => {
   it("do return post", (done) => {
@@ -20,7 +31,7 @@ describe("GET /post/:id", () => {
       .expect(200)
       .end((err, res) => {
         expect(res.body);
-        expect(res.body[0].foodName).toBe("Beef");
+        expect(res.body.foodName).toBe("Beef");
         return done();
       });
   });
