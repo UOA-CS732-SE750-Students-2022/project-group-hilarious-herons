ce2ff107c569e0c3494be8a1ffff47c3f923a7e8
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2022-04-05 08:57:56 +1200

    Like post (#63)
    * fix post.test.js
    
    * fix: post-dao update
    
    * like and unlike api
    
    * like and unlike api tests
    
    * remove console.log
    
    * fix test

--- backend/src/routes/__tests__/post.test.js
+++ backend/src/routes/__tests__/post.test.js
@@ -1,33 +1,44 @@
 const request = require("supertest");
 const express = require("express");
 const routes = require("../index");
-const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
-
-let mongod;
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use("/", routes);
 
-app.listen(80, () => console.log(`App server listening on port ${80}!`));
+mongoose
+  .connect(
+    `mongodb+srv://db-user:${process.env.MONGO_PASSWORD}@cluster0.vprvj.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then((result) => {
+    console.log("MongoDB connection successful");
+  })
+  .then(() => {
+    app.listen(80, () => console.log(`App server listening on port ${80}!`));
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed : ", error.message);
+  });
 
 describe("GET /post/:id", () => {
   it("do return post", (done) => {
     request(app)
-      .get("/post/6243c31783390bc7186d14b2")
+      .get("/post/62497099b05489667afd295c")
       .send()
       .expect(200)
       .end((err, res) => {
         expect(res.body);
-        expect(res.body[0].foodName).toBe("Beef");
+        expect(res.body.foodName).toBe("Beef");
         return done();
       });
   });
 });
 
 describe("Fail GET /post/:id", () => {
-  it("do return post", (done) => {
+  it("do not return post", (done) => {
     request(app)
       .get("/post/0")
       .send()
@@ -36,5 +47,45 @@ describe("Fail GET /post/:id", () => {
         expect(err);
         return done();
       });
+  });
+});
+
+describe("Post /post/like-post", () => {
+  it("increment likes", (done) => {
+    request(app)
+      .get("/post/62497099b05489667afd295c")
+      .send()
+      .end((err, res) => {
+        const numberOfLikes = res.body.numberOfLikes;
+        console.log(numberOfLikes);
+
+        request(app)
+          .post("/post/like-post")
+          .send(data)
+          .expect(200)
+          .end((err, res) => {
+            console.log(res.body.currentLikes);
+
+            expect(res);
+            expect(res.body.currentLikes).toBe(numberOfLikes + 1);
+          });
+
+        request(app)
+          .post("/post/unlike-post")
+          .send(data)
+          .expect(200)
+          .end((err, res) => {
+            console.log(res.body.currentLikes);
+            expect(res);
+            expect(res.body.currentLikes).toBe(
+              numberOfLikes === 0 ? 0 : numberOfLikes - 1
+            );
+            return done();
+          });
+      });
+
+    const data = {
+      id: "62497099b05489667afd295c",
+    };
   });
 });
