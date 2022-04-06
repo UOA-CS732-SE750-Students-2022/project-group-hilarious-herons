d2e857ee13ef3b9dbdd24595cca9e57eeee1882d
alyu <68038316+alyu201@users.noreply.github.com>
2022-04-06 21:02:20 +1200

    Search bar component (#60)
    * Base searchbar and filter modal layout
    
    * Setting states for sort method, rating and dietary filters
    
    * Added required css styling imports and location search function
    
    * Responsive search bar and changing to localstorage
    
    * Modified location filter processing logic
    
    * Change location icon for search filter modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
-import { IonReactRouter } from '@ionic/react-router'
-import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Redirect, Route } from "react-router-dom";
 
-import '@ionic/react/css/core.css';
-import { HomePage } from './pages/HomePage';
-import { FoodDetailsPage } from './pages/FoodDetailsPage';
-import { LoginPage } from './pages/LoginPage';
-import { AccountPage } from './pages/AccountPage';
-import { ErrorPage } from './pages/ErrorPage';
+import "@ionic/react/css/core.css";
+import { HomePage } from "./pages/HomePage";
+import { FoodDetailsPage } from "./pages/FoodDetailsPage";
+import { LoginPage } from "./pages/LoginPage";
+import { AccountPage } from "./pages/AccountPage";
+import { ErrorPage } from "./pages/ErrorPage";
 
 // /* Basic CSS for apps built with Ionic */
-// import '@ionic/react/css/normalize.css';
-// import '@ionic/react/css/structure.css';
-// import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+// import "@ionic/react/css/typography.css";
 
 // /* Optional CSS utils that can be commented out */
-// import '@ionic/react/css/padding.css';
-// import '@ionic/react/css/float-elements.css';
-// import '@ionic/react/css/text-alignment.css';
-// import '@ionic/react/css/text-transformation.css';
-// import '@ionic/react/css/flex-utils.css';
-// import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 // Required to get the app working!!!
-setupIonicReact()
+setupIonicReact();
 
 function App() {
   return (
---
+++ frontend/src/components/SearchbarFilter.js
@@ -0,0 +1,183 @@
+import {
+  IonButton,
+  IonCheckbox,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonRange,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+} from "@ionic/react";
+import { locationOutline } from "ionicons/icons";
+import { useState } from "react";
+import { LocationPopover } from "./LocationPopover";
+
+export const SearchbarFilter = ({ setShowMobileModal, doSearch }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [byDistance, setByDistance] = useState(
+    localStorage.getItem("byDistance") || false
+  );
+  const [byRating, setByRating] = useState(
+    localStorage.getItem("byRating") || false
+  );
+  const [locationFilter, setLocationFilter] = useState(
+    localStorage.getItem("locationFilter") || ""
+  );
+  const [rating, setRating] = useState(localStorage.getItem("rating") || 5);
+  const [dietaries, setDietaries] = useState(
+    JSON.parse(localStorage.getItem("dietaries")) || []
+  );
+
+  const handleReset = () => {
+    setByDistance(false);
+    setByRating(false);
+    setLocationFilter("");
+    setRating(5);
+    setDietaries([]);
+  };
+
+  const handleSearch = () => {
+    setShowModal(false);
+    setShowMobileModal(false);
+    localStorage.setItem("byDistance", byDistance);
+    localStorage.setItem("byRating", byRating);
+    localStorage.setItem("locationFilter", locationFilter);
+    localStorage.setItem("rating", rating);
+    localStorage.setItem("dietaries", JSON.stringify(dietaries));
+
+    doSearch();
+  };
+
+  const getDietaries = () => {
+    return [
+      "Vegetarian",
+      "Vegan",
+      "Gluten free",
+      "Lactose free",
+      "Halal",
+      "Dairy free",
+      "Paleo",
+    ];
+  };
+
+  return (
+    <IonModal
+      trigger="searchbar"
+      isOpen={showModal}
+      swipeToClose={true}
+      onDidPresent={() => setShowModal(true)}
+      mode="ios"
+    >
+      {/* IonHeader added to allow the swipe to close feature in ios mode */}
+      <IonHeader>
+        <IonItem lines="none" />
+      </IonHeader>
+      <IonContent>
+        <IonList lines="none">
+          <IonItemDivider>Sort by</IonItemDivider>
+          <IonItem>
+            <IonCheckbox
+              checked={byDistance}
+              slot="start"
+              onIonChange={(e) => {
+                setByDistance(e.detail.checked);
+              }}
+            />
+            <IonLabel expand="block">Shortest distance</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonCheckbox
+              checked={byRating}
+              slot="start"
+              onIonChange={(e) => {
+                setByRating(e.detail.checked);
+              }}
+            />
+            <IonLabel expand="block">Highest rating</IonLabel>
+          </IonItem>
+          <IonItemDivider>Filter by</IonItemDivider>
+          <IonItem>
+            <IonLabel>Location</IonLabel>
+            <IonSearchbar
+              style={{ width: "80%" }}
+              autocomplete="on"
+              searchIcon={locationOutline}
+              showClearButton="never"
+              id="location-searchbar"
+              value={locationFilter}
+            />
+            <LocationPopover setLocationFilter={setLocationFilter} />
+          </IonItem>
+          <IonItem>
+            <IonLabel>Rating</IonLabel>
+            <IonRange
+              max={5}
+              snaps={true}
+              pin={true}
+              value={rating}
+              style={{ minWidth: "80%" }}
+              mode="md"
+              onIonChange={(e) => {
+                setRating(e.detail.value);
+              }}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel>Dietary</IonLabel>
+            <IonSelect
+              value={dietaries}
+              multiple={true}
+              cancelText="Cancel"
+              okText="OK"
+              style={{ minWidth: "80%" }}
+              placeholder="None"
+              onIonChange={(e) => {
+                setDietaries(e.detail.value);
+              }}
+            >
+              {getDietaries().map((dietary, idx) => {
+                return (
+                  <IonSelectOption value={dietary} key={idx}>
+                    {dietary}
+                  </IonSelectOption>
+                );
+              })}
+            </IonSelect>
+          </IonItem>
+          <IonItem />
+        </IonList>
+        <IonItem lines="none">
+          <IonButton
+            expand="full"
+            fill="outline"
+            slot="end"
+            shape="round"
+            size="default"
+            onClick={() => {
+              handleReset();
+            }}
+          >
+            Reset
+          </IonButton>
+          <IonButton
+            color="primary"
+            expand="full"
+            slot="end"
+            shape="round"
+            size="default"
+            onClick={() => {
+              handleSearch();
+            }}
+          >
+            Search
+          </IonButton>
+        </IonItem>
+      </IonContent>
+    </IonModal>
+  );
+};
