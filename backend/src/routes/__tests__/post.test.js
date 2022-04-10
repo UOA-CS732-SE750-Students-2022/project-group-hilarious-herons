160e3503ea3a927845292986ad77b5813d9bdc07
Kirsty Gong <kirstygong23@gmail.com>
2022-04-10 21:29:10 +1200

    corrected the routes according to new structure

--- backend/src/routes/__tests__/post.test.js
+++ backend/src/routes/__tests__/post.test.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routes = require("../index");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
+const Post = require("../../models/Post/PostSchema");
 require("dotenv").config();
 
 const app = express();
@@ -11,20 +12,24 @@ app.use("/", routes);
 
 let mongod;
 
-// mongoose
-//   .connect(
-//     `mongodb+srv://db-user:${process.env.MONGO_PASSWORD}@cluster0.vprvj.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
-//     { useNewUrlParser: true }
-//   )
-//   .then((result) => {
-//     console.log("MongoDB connection successful");
-//   })
-//   .then(() => {
-//     app.listen(80, () => console.log(`App server listening on port ${80}!`));
-//   })
-//   .catch((error) => {
-//     console.log("MongoDB connection failed : ", error.message);
-//   });
+const mockPost = {
+  _id: new mongoose.Types.ObjectId("000000000000000000000002"),
+  foodName: "Fries",
+  bodyText: "This is a really nice Fries",
+  tags: ["Fast food", "fries"],
+  numberOfLikes: 2,
+  rating: 2,
+  numberOfReviews: 5,
+};
+const post1 = {
+  _id: new mongoose.Types.ObjectId("000000000000000000000001"),
+  foodName: "Steak",
+  bodyText: "This is a really nice steak",
+  tags: ["beef", "steak"],
+  numberOfLikes: 5,
+  rating: 10,
+  numberOfReviews: 5,
+};
 
 beforeAll(async () => {
   mongod = await MongoMemoryServer.create();
@@ -37,88 +42,45 @@ beforeEach(async () => {
   await mongoose.connection.db.dropDatabase();
 
   const coll = await mongoose.connection.db.createCollection("Posts");
-  // await coll.insertMany(breakfasts);
+  // console.log(coll);
+  const post = new Post(mockPost);
+  await post.save();
 });
+
+// afterEach(async () => {
+//   await Post.deleteMany();
+// });
 
 afterAll(async () => {
   await mongoose.disconnect();
   await mongod.stop();
 });
 
-const post1 = {
-  _id: new mongoose.Types.ObjectId("000000000000000000000001"),
-  foodName: "Steak",
-  bodyText: "This is a really nice steak",
-  tags: ["beef", "steak"],
-  numberOfLikes: 5,
-  rating: 10,
-  numberOfReviews: 5,
-};
-
-describe("GET /post/:id", () => {
+describe("GET /api/posts/:id", () => {
   it("do return post", (done) => {
     request(app)
-      .get("/post/62497099b05489667afd295c")
+      .get("/api/posts/000000000000000000000002")
       .send()
       .expect(200)
       .end((err, res) => {
+        if (err) return done(err);
         expect(res.body);
-        expect(res.body.foodName).toBe("Beef");
+        expect(res.body.foodName).toBe("Fries");
         return done();
       });
   });
 });
 
-describe("Fail GET /post/:id", () => {
+describe("Fail GET /api/post/:id", () => {
   it("do not return post", (done) => {
     request(app)
-      .get("/post/0")
+      .get("/api/posts/0")
       .send()
       .expect(404)
       .end((err) => {
         expect(err);
         return done();
       });
-  });
-});
-
-describe("Post /post/like-post", () => {
-  it("increment likes", (done) => {
-    request(app)
-      .get("/post/62497099b05489667afd295c")
-      .send()
-      .end((err, res) => {
-        const numberOfLikes = res.body.numberOfLikes;
-        console.log(numberOfLikes);
-
-        request(app)
-          .post("/post/like-post")
-          .send(data)
-          .expect(200)
-          .end((err, res) => {
-            console.log(res.body.currentLikes);
-
-            expect(res);
-            expect(res.body.currentLikes).toBe(numberOfLikes + 1);
-          });
-
-        request(app)
-          .post("/post/unlike-post")
-          .send(data)
-          .expect(200)
-          .end((err, res) => {
-            console.log(res.body.currentLikes);
-            expect(res);
-            expect(res.body.currentLikes).toBe(
-              numberOfLikes === 0 ? 0 : numberOfLikes - 1
-            );
-            return done();
-          });
-      });
-
-    const data = {
-      id: "62497099b05489667afd295c",
-    };
   });
 });
 
--- backend/src/routes/api/posts.js
+++ backend/src/routes/api/posts.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { createPost } = require("../../controllers/post");
+const {
+  createPost,
+  getPost,
+  likePost,
+  unlikePost,
+} = require("../../controllers/post");
 
 const router = express.Router();
 
 router.post("/", createPost);
+router.route("/:id").get(getPost);
+router.route("/like-post").post(likePost);
+router.route("/unlike-post").post(unlikePost);
 
 module.exports = router;
