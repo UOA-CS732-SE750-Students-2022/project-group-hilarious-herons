d1a0b19dccbd6503cabcf8038918045446013cf6
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2022-04-11 15:03:03 +1200

    Fix post apis to fit the current structure (#73)
    * corrected the routes according to new structure
    
    * changed the way to mock data
    
    * revert the previous change
    
    * chaned routing method

--- backend/src/routes/api/posts.js
+++ backend/src/routes/api/posts.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { createPost } = require("../../controllers/post");
+const {
+  createPost,
+  getPost,
+  likePost,
+  unlikePost,
+} = require("../../controllers/post");
 
 const router = express.Router();
 
 router.post("/", createPost);
+router.get("/:id", getPost);
+router.post("/like-post", likePost);
+router.post("/unlike-post", unlikePost);
 
 module.exports = router;
