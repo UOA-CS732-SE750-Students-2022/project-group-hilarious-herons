937f663003414a15719f870cfd6e9dd4caa04086
KirstyGong <kirstygong23@gmail.com>
2022-04-16 20:35:14 +1200

    created a route for image upload

--- backend/src/controllers/imageProcess.js
+++ backend/src/controllers/imageProcess.js
@@ -5,12 +5,9 @@ const { Storage } = require("@google-cloud/storage");
 const storage = new Storage({ keyFilename: "google-cloud-key.json" });
 const bucket = storage.bucket("funter_image");
 
-exports.upload = async (req) => {
+exports.upload = async (req, res) => {
   try {
-    await processFile(req);
-    if (!req.file) {
-      return res.status(400).send({ message: "Please upload a file!" });
-    }
+    await processFile(req, res);
     // Create a new blob in the bucket and upload the file data.
     const blob = bucket.file(req.file.originalname);
     const blobStream = blob.createWriteStream({
@@ -26,14 +23,22 @@ exports.upload = async (req) => {
       );
       try {
         // Make the file public
-        await bucket.file(req.file.originalname);
-      } catch (e) {
-        throw e;
+        await bucket.file(req.file.originalname).makePublic();
+      } catch {
+        return res.status(500).send({
+          message: `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
+          url: publicUrl,
+        });
       }
-      return publicUrl;
+      res.status(200).send({
+        message: "Uploaded the file successfully: " + req.file.originalname,
+        url: publicUrl,
+      });
     });
     blobStream.end(req.file.buffer);
-  } catch (e) {
-    throw e;
+  } catch (err) {
+    res.status(500).send({
+      message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+    });
   }
 };
