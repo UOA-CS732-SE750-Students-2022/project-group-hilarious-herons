f3b88a299beda4740457b705e143b10c03fa8383
henrym25 <henry3399@hotmail.co.nz>
2022-04-17 20:02:02 +1200

    Removed email and password from user schema. Now kept in firebase

--- backend/src/models/User/UserSchema.js
+++ backend/src/models/User/UserSchema.js
@@ -7,18 +7,6 @@ const userSchema = new Schema({
   displayName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: {
-    type: String,
-    required: true,
-    validate: (email) => {
-      //Validates the email by checking if it follows either 'test@test.com' or 'test@test.co.nz' format
-      return (
-        /[a-zA-z]+@[a-zA-z]+\.[a-zA-z]+\.[a-zA-z]+/.test(email) ||
-        /[a-zA-z]+@[a-zA-z]+\.[a-zA-z]+/.test(email)
-      );
-    },
-  },
-  password: { type: String, required: true },
   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   favourites: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   followingUsers: [{ type: Schema.Types.ObjectId, ref: "User" }],
