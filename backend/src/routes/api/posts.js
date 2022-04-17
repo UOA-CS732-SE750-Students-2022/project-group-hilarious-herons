437773f64515ef6958eafc369cbefed8574d951a
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2022-04-17 17:33:56 +1200

    Google cloud upload (#76)
    * image upload to cloud
    
    * created a route for image upload
    
    * Reference

--- backend/src/routes/api/posts.js
+++ backend/src/routes/api/posts.js
@@ -5,6 +5,7 @@ const {
   likePost,
   unlikePost,
 } = require("../../controllers/post");
+const { upload } = require("../../controllers/imageProcess");
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.post("/", createPost);
 router.get("/:id", getPost);
 router.post("/like-post", likePost);
 router.post("/unlike-post", unlikePost);
+router.post("/image", upload);
 
 module.exports = router;
