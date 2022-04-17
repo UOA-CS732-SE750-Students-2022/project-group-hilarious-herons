54ae251a3fd98953d8b146242c8bea3584f44986
henrym25 <henry3399@hotmail.co.nz>
2022-04-18 03:59:51 +1200

    Added users base route

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const posts = require("./posts");
+const users = require("./users");
 
 const router = express.Router();
 
 router.use("/posts", posts);
+router.use("/users", users);
 
 module.exports = router;
