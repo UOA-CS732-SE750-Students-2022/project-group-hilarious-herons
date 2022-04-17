48b702d3f3cd001303da293eb58ebdd212ac92fa
henrym25 <henry3399@hotmail.co.nz>
2022-04-17 18:07:12 +1200

    Added firebaseUUID to user schema

--- backend/src/models/User/UserSchema.js
+++ backend/src/models/User/UserSchema.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firebaseUUID: { type: String, required: true },
   displayName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
