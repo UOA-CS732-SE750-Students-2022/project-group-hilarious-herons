2d2092c865690f90b6a1eb74e177e1b976569c24
henrym25 <henry3399@hotmail.co.nz>
2022-04-17 18:07:31 +1200

    Added user dao functions

---
+++ backend/src/models/User/user-dao.js
@@ -0,0 +1,54 @@
+const User = require("./UserSchema");
+
+/**
+ * Saves a user to the database
+ * @param {*} user The user object to be saved
+ * @returns User object created from mongoose schema
+ */
+exports.createUser = async (user) => {
+  const newUser = new User(user);
+  await newUser.save();
+
+  return newUser;
+};
+
+/**
+ * Retrieves the user from the database with the specified ID
+ * @param {*} userID UUID for the restaurant to be retrieved (This is MONGODB ID)
+ * @returns Object with given userID if it can be found
+ */
+exports.retrieveUser = async (userID) => {
+  return await User.findById(userID);
+};
+
+/**
+ * Updates an existing user in the database with new data from supplied object
+ * @param {*} user A user object data to update object
+ * @returns True if object is found and update, false otherwise
+ */
+exports.updateUser = async (user) => {
+  const existingUser = await User.findById(user._id);
+
+  if (existingUser) {
+    existingUser.displayName = user.displayName;
+    existingUser.firstName = user.firstName;
+    existingUser.lastName = user.lastName;
+    existingUser.posts = user.posts;
+    existingUser.favourites = user.favourites;
+    existingUser.followingUsers = user.followingUsers;
+
+    await existingUser.save();
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Deletes the user from the database, if it exists.
+ * @param {*} user The ID for the restaurant to be deleted
+ * @returns
+ */
+exports.deleteUser = async (userID) => {
+  return await User.deleteOne({ id: userID });
+};
