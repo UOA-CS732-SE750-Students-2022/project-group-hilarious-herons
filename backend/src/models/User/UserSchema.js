11400c7e31154f537a00e76fcdbf79356a590424
hman25 <67317842+henrym25@users.noreply.github.com>
2022-04-18 19:03:11 +1200

    Create tests for models (#77)
    * Added firebaseUUID to user schema
    
    * Added user dao functions
    
    * Added restaurant schema tests
    
    * Created tests for post schema
    
    * Removed email and password from user schema. Now kept in firebase
    
    * Added tests for user schema
    
    * Refactored directory structure
    
    * Get restaurant detail (#74)
    
    * added open hours to restaurant schema
    
    * changed the openHours format
    
    * getRestaurant api
    
    * change to a get method
    
    * added test
    
    * prevent adding duplicated restaurant to db
    
    * dealing with restaurant not found
    
    * added open hours to restaurant schema
    
    * changed the openHours format
    
    * getRestaurant api
    
    * change to a get method
    
    * added test
    
    * prevent adding duplicated restaurant to db
    
    * dealing with restaurant not found
    
    * Changing collection name in test to default names from mongodb
    
    * update the test
    
    * fixed test
    
    Co-authored-by: henrym25 <henry3399@hotmail.co.nz>
    
    * Added firebaseUUID to user schema
    
    * Added user dao functions
    
    * Added restaurant schema tests
    
    * Created tests for post schema
    
    * Removed email and password from user schema. Now kept in firebase
    
    * Added tests for user schema
    
    * Refactored directory structure
    
    Co-authored-by: KirstyGong <66720955+KirstyGong@users.noreply.github.com>

--- backend/src/models/User/UserSchema.js
+++ backend/src/models/User/UserSchema.js
@@ -3,21 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firebaseUUID: { type: String, required: true },
   displayName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: {
-    type: String,
-    required: true,
-    validate: (email) => {
-      //Validates the email by checking if it follows either 'test@test.com' or 'test@test.co.nz' format
-      return (
-        /[a-zA-z]+@[a-zA-z]+\.[a-zA-z]+\.[a-zA-z]+/.test(email) ||
-        /[a-zA-z]+@[a-zA-z]+\.[a-zA-z]+/.test(email)
-      );
-    },
-  },
-  password: { type: String, required: true },
   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   favourites: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   followingUsers: [{ type: Schema.Types.ObjectId, ref: "User" }],
