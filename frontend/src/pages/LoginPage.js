f15112e0abb0c9b5a79b600d1bb47a7b82bcaf8c
Yujia Wu <ywu660@aucklanduni.ac.nz>
2022-04-18 19:59:28 +0800

    Implement the login page website version.

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,50 @@
-import { IonPage } from "@ionic/react";
-import { ActionHeader } from "../components/ActionHeader";
+import { 
+  IonPage, 
+  IonContent, 
+  IonTitle, 
+  IonButton, 
+  IonIcon, 
+  IonText,
+  IonRow,
+  IonGrid,
+} from "@ionic/react";
+import { 
+  logoGoogle
+} from 'ionicons/icons';
+import { useState } from "react";
+import './LoginPage.css';
 
 export const LoginPage = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <IonPage>
-      <ActionHeader />
+      <IonContent fullscreen className="login-content">
+        <div className="component">
+          <div className="componentx"> 
+            <IonTitle>Log in to your account!</IonTitle>
+            <br /> 
+            <br /> 
+            <IonButton className="login-button" expand="full" size="large">
+              <IonIcon slot="start"  icon={ logoGoogle } color="light"/>
+                Login with Google
+            </IonButton>   
+            <br /> 
+            <br /> 
+            <br /> 
+            <IonRow style={{ display: "inline-block" }} className="sign-up">
+              <IonText style={{ fontWeight: "bold"}}>Don't have an account?&nbsp;&nbsp;</IonText>
+              <IonButton style={{ verticalAlign: "middle" }} mode="ios" size="small">Sign Up</IonButton>  
+            </IonRow>
+          </div>
+        </div>
+        <div className="image-content">
+          <IonText style={{ color: "white"}} >
+            <h1>FUNTER</h1>
+          </IonText>
+        </div>
+
+      </IonContent>
     </IonPage>
   );
 };
