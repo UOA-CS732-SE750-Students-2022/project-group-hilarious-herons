c598a6929de7dfe306b1845cc0697aec5cfff9e7
KirstyGong <kirstygong23@gmail.com>
2022-04-19 16:05:45 +1200

    return post from database

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -3,8 +3,13 @@ const {
   updatePost,
   createPost,
 } = require("../models/Post/posts-dao");
+const Restaurant = require("../models/Restaurant/RestaurantSchema");
 const { retrieveRestaurant } = require("../models/Restaurant/restaurant-dao");
 const mongoose = require("mongoose");
+const {
+  distanceCalculation,
+} = require("../utils/distance/distanceCalculation");
+const Post = require("../models/Post/PostSchema");
 
 exports.getPost = async (req, res) => {
   try {
@@ -114,6 +119,54 @@ exports.createPost = async (req, res) => {
       .json(newPost);
   } catch (err) {
     res.status(500).json({
+      success: false,
+      info: err.message,
+    });
+  }
+};
+
+const getPostsFromDB = async (lat, long, range) => {
+  const response = await Restaurant.find({});
+
+  let distancesObj = [];
+  for (i of response) {
+    const distLat = i.coordinates.lat;
+    const distLong = i.coordinates.long;
+    const distance = distanceCalculation(lat, long, distLat, distLong);
+    if (distance < range) {
+      // const response = await Post.find({ restaurant: i._id });
+      // console.log(response);
+      distancesObj = [...distancesObj, { distance: distance, id: i._id }];
+      // return i._id;
+    }
+  }
+
+  distancesObj.sort((a, b) => {
+    return a.distance - b.distance;
+  });
+
+  let posts = [];
+  for (i in distancesObj) {
+    const response = await Post.find({ restaurant: i._id });
+    posts = [...posts, response];
+  }
+  return posts;
+};
+
+exports.getPosts = async (req, res) => {
+  try {
+    const { lat, long, desktop } = req.body;
+    let { range } = req.body;
+
+    if (!range) {
+      range = 10000;
+    }
+
+    let posts = await getPostsFromDB(lat, long, range);
+
+    return res.send(posts);
+  } catch (err) {
+    return res.status(500).json({
       success: false,
       info: err.message,
     });
--- backend/src/routes/api/posts.js
+++ backend/src/routes/api/posts.js
@@ -4,11 +4,13 @@ const {
   getPost,
   likePost,
   unlikePost,
+  getPosts,
 } = require("../../controllers/post");
 const { upload } = require("../../controllers/imageProcess");
 
 const router = express.Router();
 
+router.get("/", getPosts);
 router.post("/", createPost);
 router.get("/:id", getPost);
 router.post("/like-post", likePost);
