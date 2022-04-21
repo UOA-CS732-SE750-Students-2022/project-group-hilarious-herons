30bdb96423a7fc2a93ccca1c04e6ae12220a18c2
alyu <68038316+alyu201@users.noreply.github.com>
2022-04-21 17:40:59 +1200

    AddPostModal enhancements (#82)
    * Add post tag input
    
    * Fix and add restaurant location dropdown
    
    * Fix adding tags to post

--- frontend/src/components/LocationPopover.js
+++ frontend/src/components/LocationPopover.js
@@ -1,86 +1,60 @@
 import {
+  IonCard,
+  IonCardContent,
   IonCardSubtitle,
   IonContent,
   IonItem,
   IonLabel,
   IonList,
-  IonPopover,
   IonText,
 } from "@ionic/react";
 
-export const LocationPopover = ({ setLocationFilter }) => {
-  const getLocations = () => {
-    const locations = [
-      "9 Pah Road, Epsom, Auckland 1023",
-      "Level 1/12 Wyndham Street, Auckland CBD, Auckland 1010",
-      "Saint Patricks Square, Wyndham Street, Auckland CBD, Auckland 1010",
-      "375 Karangahape Road, Auckland CBD, Auckland 1010",
-      "210 Symonds Street, Eden Terrace, Auckland 1010",
-      "86 Federal Street, Auckland CBD, Auckland 1010",
-      "90 Federal Street, Auckland CBD, Auckland 1010",
-      "491F Pakuranga Road, Highland Park, Half Moon Bay, Auckland 2010",
-      "63 Davis Crescent, Newmarket, Auckland 1023",
-      "3d Short Street, Newmarket, Auckland 1023",
-      "10-12 Teed Street, Newmarket, Auckland 1023",
-      "854 Dominion Road, Mount Eden, Auckland 1041",
-      "479 New North Road, Kingsland, Auckland 1021",
-      "103-113 Westhaven Drive, St Marys Bay, Auckland 1144",
-      "70 Jervois Road, Ponsonby, Auckland 1011",
-      "417 Tamaki Drive, St Heliers, Auckland 1071",
-      "272D Ti Rakau Drive, Pakuranga, Auckland 2013",
-      "64 Waimarie Street, St Heliers, Auckland 1071",
-      "47 The Parade, Bucklands Beach, Auckland 2012",
-    ].map((address) => {
-      return {
-        region: address.split(",")[1].trim(),
-        city: address.split(",")[2].trim().split(" ")[0],
-      };
-    });
-
-    const uniqLocations = [];
-    locations.forEach((loc) => {
-      const hasLocation =
-        uniqLocations.filter((uniqLoc) => {
-          return loc.region === uniqLoc.region && loc.city === uniqLoc.city; // finds matching locations
-        }).length > 0;
-
-      if (!hasLocation) {
-        uniqLocations.push(loc);
-      }
-    });
-
-    return uniqLocations;
-  };
-
+export const LocationPopover = ({ setLocationText, locations, show }) => {
+  const items = locations.map((location) => {
+    return {
+      heading: Object.values(location)[0],
+      subHeading: Object.values(location)[1],
+    };
+  });
   return (
-    <IonPopover
-      alignment="center"
-      trigger="location-searchbar"
-      size="cover"
-      showBackdrop={false}
-      dismissOnSelect={true}
+    <IonCard
+      style={{
+        position: "absolute",
+        zIndex: "10",
+        width: "75%",
+        margin: "0 0 0 20%",
+        display: show ? "initial" : "none",
+      }}
     >
-      <IonContent style={{ maxHeight: "12.5rem" }}>
-        <IonList>
-          {getLocations().map(({ region, city }, idx) => {
-            return (
-              <IonItem
-                button
-                key={idx}
-                lines="none"
-                onClick={() => {
-                  setLocationFilter(region);
-                }}
-              >
-                <IonText>
-                  <IonLabel>{region}</IonLabel>
-                  <IonCardSubtitle>{city}, New Zealand</IonCardSubtitle>
-                </IonText>
-              </IonItem>
-            );
-          })}
-        </IonList>
-      </IonContent>
-    </IonPopover>
+      <IonCardContent
+        style={{
+          height: "7.5rem",
+          padding: "1rem 0",
+        }}
+      >
+        <IonContent>
+          <IonList>
+            {items.map(({ heading, subHeading }, idx) => {
+              return (
+                <IonItem
+                  button
+                  key={idx}
+                  lines="none"
+                  onClick={() => {
+                    setLocationText(heading);
+                  }}
+                  mode="md"
+                >
+                  <IonText>
+                    <IonLabel>{heading}</IonLabel>
+                    <IonCardSubtitle>{subHeading}, New Zealand</IonCardSubtitle>
+                  </IonText>
+                </IonItem>
+              );
+            })}
+          </IonList>
+        </IonContent>
+      </IonCardContent>
+    </IonCard>
   );
 };
