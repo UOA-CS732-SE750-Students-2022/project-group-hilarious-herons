0acd0a86c7402ecc1e03300a57a1e31539e50af6
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-04-22 23:08:18 +1200

    [refactor]: Refactored helper.
    Refactored helper function to reduce duplicate code.

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,17 +1,5 @@
 export const getTodaysDateAsString = () => {
-    const currentDate = new Date()
-    let dateVal = currentDate.getDate().toString()
-    let date = currentDate.getDate().toString()
-    if (dateVal < 10) {
-        date = "0" + month
-    }
-    let monthVal = currentDate.getMonth()
-    let month = currentDate.getMonth().toString()
-    if (monthVal < 10) {
-        month = "0" + month
-    }
-    const formattedDate = date + "/" + month + "/" + currentDate.getFullYear().toString()
-    return formattedDate
+    return getDateAsString(new Date())
 }
 
 export const getDateAsString = (providedDate) => {
