834fb829015d70801d309aa8ecfa9bc05f0475a0
hman25 <67317842+henrym25@users.noreply.github.com>
2022-04-23 17:44:17 +1200

    Create user backend routes (#79)
    * Added firebaseUUID to user schema
    
    * Added user dao functions
    
    * Added restaurant schema tests
    
    * Created tests for post schema
    
    * Removed email and password from user schema. Now kept in firebase
    
    * Added tests for user schema
    
    * Added user controller functions
    
    * Implemented user routes
    
    * Added users base route
    
    * Added user endpoint tests
    
    * fixed test failing

---
+++ backend/src/controllers/user.js
@@ -0,0 +1,65 @@
+const {
+  createUser,
+  retrieveUser,
+  updateUser,
+} = require("../models/User/user-dao");
+
+exports.createUser = async (req, res) => {
+  try {
+    const userObj = req.body;
+    const newUser = await createUser(userObj);
+
+    res
+      .status(201)
+      .header("Location", `/api/user/${newUser._id}`)
+      .json(newUser);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      info: err.message,
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await retrieveUser(id);
+
+    if (user === undefined || user === null || user.length === 0) {
+      res.status(404);
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      info: e.message,
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const userID = req.params.id;
+    const user = await retrieveUser(userID);
+
+    if (user === undefined || user === null || user.length === 0) {
+      res.sendStatus(404);
+    }
+
+    const updatedUserObj = req.body;
+    updatedUserObj._id = userID;
+
+    updateUser(updatedUserObj);
+
+    res.status(204).json({
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      info: err.message,
+    });
+  }
+};
---
+++ backend/src/routes/__tests__/users.test.js
@@ -0,0 +1,216 @@
+const request = require("supertest");
+const express = require("express");
+const routes = require("../index");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+const User = require("../../models/User/UserSchema");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use("/", routes);
+
+let mongod;
+
+let mockUser = {
+  _id: new mongoose.Types.ObjectId("000000000000000000000001"),
+  firebaseUUID: "ID_1",
+  displayName: "user1",
+  firstName: "first",
+  lastName: "user",
+  posts: [new mongoose.Types.ObjectId("000000000000000000000001")],
+  favourites: [new mongoose.Types.ObjectId("000000000000000000000001")],
+  followingUsers: [new mongoose.Types.ObjectId("000000000000000000000001")],
+};
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+
+  const connectionString = mongod.getUri();
+  await mongoose.connect(connectionString, { useNewUrlParser: true });
+});
+
+beforeEach(async () => {
+  await mongoose.connection.db.dropDatabase();
+
+  const coll = await mongoose.connection.db.createCollection("users");
+  const user = new User(mockUser);
+  await user.save();
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongod.stop();
+});
+
+describe("GET user /api/user/:id", () => {
+  it("successful get user", (done) => {
+    request(app)
+      .get("/api/users/000000000000000000000001")
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        expect(res.body);
+        expect(res.body.displayName).toBe("user1");
+        expect(res.body.firstName).toBe("first");
+        expect(res.body.lastName).toBe("user");
+
+        return done();
+      });
+  });
+
+  it("user does not exist", (done) => {
+    request(app)
+      .get("/api/users/000000000000000000000002")
+      .send()
+      .expect(404)
+      .end((err, res) => {
+        expect(err);
+        return done();
+      });
+  });
+});
+
+describe("POST user /api/users/", () => {
+  it("post valid user", (done) => {
+    const newUser = {
+      firebaseUUID: "ID_2",
+      displayName: "user2",
+      firstName: "second",
+      lastName: "person",
+      posts: ["000000000000000000000001"],
+      favourites: ["000000000000000000000001"],
+      followingUsers: ["000000000000000000000001"],
+    };
+
+    request(app)
+      .post("/api/users")
+      .set("Accept", "application/json")
+      .send(newUser)
+      .expect(201)
+      .end((err, res) => {
+        expect(res.body);
+        expect(res.body.displayName).toBe("user2");
+        expect(res.body.firstName).toBe("second");
+        expect(res.body.lastName).toBe("person");
+        return done();
+      });
+  });
+
+  it("post invalid user (missing firebase UUID)", (done) => {
+    const newUser = {
+      firebaseUUID: "",
+      displayName: "user2",
+      firstName: "second",
+      lastName: "person",
+      posts: ["000000000000000000000001"],
+      favourites: ["000000000000000000000001"],
+      followingUsers: ["000000000000000000000001"],
+    };
+
+    request(app)
+      .post("/api/users")
+      .set("Accept", "application/json")
+      .send(newUser)
+      .expect(500)
+      .end((err, res) => {
+        expect(err);
+        return done();
+      });
+  });
+
+  it("post invalid user (missing display name)", (done) => {
+    const newUser = {
+      firebaseUUID: "ID_2",
+      displayName: "",
+      firstName: "second",
+      lastName: "person",
+      posts: ["000000000000000000000001"],
+      favourites: ["000000000000000000000001"],
+      followingUsers: ["000000000000000000000001"],
+    };
+
+    request(app)
+      .post("/api/users")
+      .set("Accept", "application/json")
+      .send(newUser)
+      .expect(500)
+      .end((err, res) => {
+        expect(err);
+        return done();
+      });
+  });
+
+  it("post invalid user (missing first name)", (done) => {
+    const newUser = {
+      firebaseUUID: "ID_2",
+      displayName: "user2",
+      firstName: "",
+      lastName: "person",
+      posts: ["000000000000000000000001"],
+      favourites: ["000000000000000000000001"],
+      followingUsers: ["000000000000000000000001"],
+    };
+
+    request(app)
+      .post("/api/users")
+      .set("Accept", "application/json")
+      .send(newUser)
+      .expect(500)
+      .end((err, res) => {
+        expect(err);
+        return done();
+      });
+  });
+
+  it("post invalid user (missing last name)", (done) => {
+    const newUser = {
+      firebaseUUID: "ID_2",
+      displayName: "user2",
+      firstName: "second",
+      lastName: "",
+      posts: ["000000000000000000000001"],
+      favourites: ["000000000000000000000001"],
+      followingUsers: ["000000000000000000000001"],
+    };
+
+    request(app)
+      .post("/api/users")
+      .set("Accept", "application/json")
+      .send(newUser)
+      .expect(500)
+      .end((err, res) => {
+        expect(err);
+        return done();
+      });
+  });
+});
+
+describe("PUT user /api/users/:id", () => {
+  it("successful update user", async () => {
+    const updatedUser = {
+      firebaseUUID: "ID_1",
+      displayName: "user1",
+      firstName: "updated",
+      lastName: "user",
+      posts: ["000000000000000000000001"],
+      favourites: ["000000000000000000000001"],
+      followingUsers: ["000000000000000000000001"],
+    };
+
+    await request(app)
+      .put("/api/users/000000000000000000000001")
+      .set("Accept", "application/json")
+      .send(updatedUser)
+      .expect(204);
+
+    const { body: updatedDBUser } = await request(app)
+      .get("/api/users/000000000000000000000001")
+      .send()
+      .expect(200);
+
+    expect(updatedDBUser.firstName).toBe("updated");
+  });
+});
