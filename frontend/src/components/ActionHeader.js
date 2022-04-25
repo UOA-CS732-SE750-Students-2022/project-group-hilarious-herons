525997eb67c5e0865ea8c5f1eb4e9c3c57ae468e
alyu201 <alyu201@aucklanduni.ac.nz>
2022-04-26 00:09:43 +1200

    Fixed bug with small number of items

--- frontend/src/components/ActionHeader.js
+++ frontend/src/components/ActionHeader.js
@@ -12,53 +12,34 @@ import { createAnimation } from "@ionic/react";
 import { useState } from "react";
 import { Searchbar } from "./Searchbar";
 import { UserPopover } from "./UserPopover";
+import "./ActionHeader.css";
 
 export const ActionHeader = ({ banner, children }) => {
   const bannerUrl = `url("https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`;
   const [headerVisible, setHeaderVisible] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const shrinkAnimation = createAnimation()
-    .addElement(document.querySelector(".toolbar"))
-    .duration(500)
-    .easing("ease-out")
-    .fromTo("height", "300px", "0")
-    .fromTo("transform", "translateY(0px)", "translateY(-500px)")
-    .fromTo("opacity", "1", "0");
-
-  const growAnimation = createAnimation()
-    .addElement(document.querySelector(".toolbar"))
-    .duration(500)
-    .easing("ease-in")
-    .fromTo("height", "0", "300px")
-    .fromTo("transform", "translateY(-300px)", "translateY(0px)")
-    .fromTo("opacity", "0", "1");
-
   const colorInAnimation = createAnimation()
-    .addElement(document.querySelector(".header"))
-    .duration(300)
+    .addElement(document.querySelector("#header"))
+    .duration(200)
     .easing("ease-in")
     .to("background", "#ff9f1c");
 
   const colorOutAnimation = createAnimation()
-    .addElement(document.querySelector(".header"))
-    .duration(300)
+    .addElement(document.querySelector("#header"))
+    .duration(200)
     .easing("ease-out")
     .to("background", `transparent`);
 
   const handleScroll = (event) => {
-    if (event.detail.scrollTop === 0 && !headerVisible) {
-      setHeaderVisible(true);
-      growAnimation.play();
-      if (banner) {
+    if (banner) {
+      if (event.detail.scrollTop === 0 && !headerVisible) {
+        setHeaderVisible(true);
         colorOutAnimation.play();
       }
-    }
 
-    if (event.detail.scrollTop > 0 && headerVisible) {
-      setHeaderVisible(false);
-      shrinkAnimation.play();
-      if (banner) {
+      if (event.detail.scrollTop > 0 && headerVisible) {
+        setHeaderVisible(false);
         colorInAnimation.play();
       }
     }
@@ -66,58 +47,60 @@ export const ActionHeader = ({ banner, children }) => {
 
   return (
     <IonContent
+      style={{ minHeight: "50rem" }}
       scrollEvents={true}
       onIonScroll={(event) => handleScroll(event)}
     >
-      <div
-        style={{
-          width: "100%",
-          background: banner
-            ? `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), ${bannerUrl}`
-            : "#ff9f1c",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
-        <IonToolbar
-          color="transparent"
-          class="header"
-          style={{ position: "fixed" }}
+      <div>
+        <div
+          className="header-bg"
+          style={{
+            background: banner
+              ? `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), ${bannerUrl}`
+              : "#ff9f1c",
+          }}
+        />
+        <div
+          className="header"
+          id="header"
+          style={{ background: banner ? "transparent" : "#ff9f1c" }}
         >
-          <IonText slot="start" style={{ color: "white", margin: "0 5%" }}>
-            <h2>FUNTER</h2>
-          </IonText>
+          <IonToolbar color="transparent">
+            <IonText slot="start" style={{ color: "white", margin: "0 5%" }}>
+              <h2>FUNTER</h2>
+            </IonText>
 
-          <Searchbar />
+            <Searchbar />
 
-          {isLoggedIn ? (
-            <IonChip
-              id="user-avatar"
-              slot="end"
-              color="light"
-              style={{ margin: "0 5%" }}
-            >
-              <IonAvatar>
-                <img src="https://ionicframework.com/docs/demos/api/avatar/avatar.svg" />
-              </IonAvatar>
-              <IonLabel>Username</IonLabel>
-            </IonChip>
-          ) : (
-            <IonButton
-              slot="end"
-              shape="round"
-              color="light"
-              style={{ margin: "0 5%" }}
-              mode="ios"
-            >
-              Login
-            </IonButton>
-          )}
-          <UserPopover />
-        </IonToolbar>
+            {isLoggedIn ? (
+              <IonChip
+                id="user-avatar"
+                slot="end"
+                color="light"
+                style={{ margin: "0 5%" }}
+              >
+                <IonAvatar>
+                  <img src="https://ionicframework.com/docs/demos/api/avatar/avatar.svg" />
+                </IonAvatar>
+                <IonLabel>Username</IonLabel>
+              </IonChip>
+            ) : (
+              <IonButton
+                slot="end"
+                shape="round"
+                color="light"
+                style={{ margin: "0 5%" }}
+                mode="ios"
+              >
+                Login
+              </IonButton>
+            )}
+            <UserPopover />
+          </IonToolbar>
+        </div>
 
         {banner ? (
-          <IonToolbar class="toolbar" color="transparent">
+          <header className="banner">
             <IonTitle
               slot="start"
               style={{ fontSize: "2.25em", color: "white", margin: "5%" }}
@@ -128,11 +111,10 @@ export const ActionHeader = ({ banner, children }) => {
                 HUNTING JOURNEY.
               </h1>
             </IonTitle>
-          </IonToolbar>
+          </header>
         ) : null}
+        {children}
       </div>
-
-      <div style={{ minHeight: "50rem" }}>{children}</div>
     </IonContent>
   );
 };
