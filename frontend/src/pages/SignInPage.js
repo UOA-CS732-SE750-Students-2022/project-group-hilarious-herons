3473248613734da927b5a61029752882926e75a3
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2022-05-08 10:31:07 +1200

    responsive login screen (#102)

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -6,11 +6,14 @@ import {
   IonIcon,
   IonText,
   useIonToast,
+  IonRow,
+  IonCol,
 } from "@ionic/react";
 import { logoGoogle } from "ionicons/icons";
 import "./SignInPage.css";
 import { signIn } from "../firebase";
-import { userService } from '../services/UserService';
+import { userService } from "../services/UserService";
+// import loginPage from "../asset/login.jpg";
 
 export const SignInPage = () => {
   const [present] = useIonToast();
@@ -21,15 +24,15 @@ export const SignInPage = () => {
       color: "dark",
       duration: 2000,
     });
-  }
+  };
   const SignInWithGoogle = () => {
     signIn(async (ok, user) => {
       if (ok) {
         const dbUser = await userService.getUser(user.uid);
 
-        if(!dbUser) {
+        if (!dbUser) {
           createWarning("Not start the backend");
-          return ;
+          return;
         }
 
         if (dbUser === 404) {
@@ -42,63 +45,47 @@ export const SignInPage = () => {
             favourites: [],
             followingUsers: [],
           };
-           await userService.createUser(newUserObj);
+          await userService.createUser(newUserObj);
         }
-        localStorage.setItem('isLoggedIn', true);
+        localStorage.setItem("isLoggedIn", true);
         localStorage.setItem("displayName", user.displayName);
-        localStorage.setItem('uid', user.uid);
-        window.location.href = '/';
+        localStorage.setItem("uid", user.uid);
+        window.location.href = "/";
       } else {
         createWarning("Signin with Google Unsuccessfully");
       }
-      
     });
   };
 
   return (
     <IonPage>
-      <IonContent fullscreen className="login-content">
-        <div className="component">
-          <div className="componentx">
-            <IonTitle>Log in to your account!</IonTitle>
-            <br />
-            <br />
-            <IonButton
-              className="login-button"
-              size="large"
-              mode="ios"
-              onClick={SignInWithGoogle}
-            >
-              <IonIcon slot="start" icon={logoGoogle} color="light" />
-              Sign In with Google
-            </IonButton>
-            <br />
-            <br />
-            <br />
-            {/* <IonRow className="sign-up">
-              <IonText>Don't have an account?&nbsp;&nbsp;</IonText>
-              <IonButton 
-                mode="ios" 
-                size="small" 
-                onClick={}
-              >
-                Sign Up
-              </IonButton>
-            </IonRow> */}
-          </div>
-        </div>
-        <div className="image-content">
-          <IonText>
-            <h1>FUNTER</h1>
-          </IonText>
-          <IonTitle slot="start">
-            <h1>
-              START YOUR FOOD
-              <br />
-              HUNTING JOURNEY.
-            </h1>
-          </IonTitle>
-        </div>
+      <IonContent>
+        <IonRow className="grid">
+          <IonCol style={{ padding: 0 }} className="image-content" size="12">
+            <IonRow>
+              <IonCol size="0" sizeXs="12">
+                <IonTitle>
+                  <h2>FUNTER</h2>
+                </IonTitle>
+              </IonCol>
+
+              <IonCol size="12" className="title-text-spacing">
+                <IonTitle className="description-text">
+                  <h1>
+                    START YOUR FOOD
+                    <br /> HUNTING JOURNEY
+                  </h1>
+                </IonTitle>
+              </IonCol>
+              <IonCol className="small-screen-login-button">
+                <IonButton size="large" mode="ios" onClick={SignInWithGoogle}>
+                  <IonIcon slot="start" icon={logoGoogle} color="light" />
+                  Sign In with Google
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
