08f2ae082321008aca5b8e5abeacd952daf5c18d
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-05-08 23:26:00 +1200

    [fix]: FoodCard parameters added.
    FoodCard parameters added to load in data given.

--- frontend/src/components/PostsLayout.js
+++ frontend/src/components/PostsLayout.js
@@ -1,14 +1,14 @@
 import { IonGrid, IonRow, IonCol } from '@ionic/react';
 import FoodCard from './FoodCard';
 
-export const PostsLayout = ({ dataForCards=[] }) => {
+export const PostsLayout = ({ dataForCards = [] }) => {
     return (
         <IonGrid>
             <IonRow>
-                {dataForCards.map((cardData) => {
+                {dataForCards.map((cardData, index) => {
                     return (
-                        <IonCol size='auto'>
-                            <FoodCard />
+                        <IonCol size='auto' key={index}>
+                            <FoodCard id={cardData.id} foodName={cardData.foodName} rating={cardData.rating} timestamp={cardData.timestamp} numberOfLikes={cardData.numberOfLikes} postLiked={cardData.postLiked} />
                         </IonCol>
                     );
                 })}
