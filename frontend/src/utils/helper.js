a67e3b92830f0bbc2dc1e7502373b2ade55bf27d
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-05-08 23:17:48 +1200

    [fix]: GetMonth.
    Get Month indexes from 0, so add 1.

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -4,7 +4,7 @@ export const getTodaysDateAsString = () => {
 
 export const getDateAsString = (providedDate) => {
     let date = ('0' + providedDate.getDate()).slice(-2)
-    let month = ('0' + providedDate.getMonth()).slice(-2)
+    let month = ('0' + (providedDate.getMonth()+1)).slice(-2)
     const formattedDate = date + "/" + month + "/" + providedDate.getFullYear().toString()
     return formattedDate
 }
