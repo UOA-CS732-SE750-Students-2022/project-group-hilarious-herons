647771f89bcb93bd6b5d2c15f271f06d979cb13a
osamakashif <osamakashif1@hotmail.com>
2022-05-09 10:43:50 +1200

    [refactor]: Getting timestamp from id.
    Refactored. Functionality for getting timestamp from id moved to separate function.

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -4,7 +4,7 @@ export const getTodaysDateAsString = () => {
 
 export const getDateAsString = (providedDate) => {
     let date = ('0' + providedDate.getDate()).slice(-2)
-    let month = ('0' + (providedDate.getMonth()+1)).slice(-2)
+    let month = ('0' + (providedDate.getMonth() + 1)).slice(-2)
     const formattedDate = date + "/" + month + "/" + providedDate.getFullYear().toString()
     return formattedDate
 }
@@ -15,10 +15,17 @@ export const formatLocaleString = (localeDateString) => {
     let day = splitString[1]
     let year = splitString[2]
     if (month.length === 1) {
-        month = "0"+month
+        month = "0" + month
     }
     if (day.length === 1) {
-        day = "0"+day
+        day = "0" + day
     }
-    return day+"/"+month+"/"+year
+    return day + "/" + month + "/" + year
+}
+
+export const getTimestampFromId = (id) => {
+    let timestamp = id.toString().substring(0, 8)
+    let date = new Date(parseInt(timestamp, 16) * 1000)
+    let formattedDate = formatLocaleString(date.toLocaleDateString())
+    return formattedDate
 }
--- frontend/src/utils/postManager.js
+++ frontend/src/utils/postManager.js
@@ -1,5 +1,5 @@
 import { PostService } from "../services/PostService"
-import { formatLocaleString } from "./helper"
+import { getTimestampFromId } from "./helper"
 
 export const postDataForFoodCard = async (bodyJson = {
     "lat": -36.91042,
@@ -10,9 +10,7 @@ export const postDataForFoodCard = async (bodyJson = {
     let dataToReturn = []
     const result = await PostService.getPosts(bodyJson)
     result.forEach((data, index) => {
-        let timestamp = data._id.toString().substring(0, 8)
-        let date = new Date(parseInt(timestamp, 16) * 1000)
-        let formattedDate = formatLocaleString(date.toLocaleDateString())
+        let formattedDate = getTimestampFromId(data._id)
         let jsonForFoodCard = {
             id: data._id,
             foodName: data.foodName,
