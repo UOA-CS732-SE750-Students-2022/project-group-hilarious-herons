afb0d7e53ba901f60e3bc9567dac2ab19cc69f32
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-05-10 16:15:57 +1200

    Removed no image posts from PostsLayout. (#111)
    * [feat]: Adding image to FoodCard.
    
    FoodCard shows image passed to it now.
    
    * [fix]: Fallback image and rendering.
    
    Fallback image provided if attempted to render without image.
    If no image from database, then post not rendered.
    
    * [docs]: README updated.
    
    Updated README, fixed some typos and added a word.

--- frontend/src/components/PostsLayout.js
+++ frontend/src/components/PostsLayout.js
@@ -6,11 +6,15 @@ export const PostsLayout = ({ dataForCards = [] }) => {
         <IonGrid>
             <IonRow>
                 {dataForCards.map((cardData, index) => {
-                    return (
-                        <IonCol size='auto' key={index}>
-                            <FoodCard id={cardData.id} image={cardData.image} foodName={cardData.foodName} rating={cardData.rating} timestamp={cardData.timestamp} numberOfLikes={cardData.numberOfLikes} postLiked={cardData.postLiked} />
-                        </IonCol>
-                    );
+                    if (typeof cardData.image !== 'undefined') {
+                        return (
+                            <IonCol size='auto' key={index}>
+                                <FoodCard id={cardData.id} image={cardData.image} foodName={cardData.foodName} rating={cardData.rating} timestamp={cardData.timestamp} numberOfLikes={cardData.numberOfLikes} postLiked={cardData.postLiked} />
+                            </IonCol>
+                        );
+                    } else {
+                        return (<IonCol key={index}></IonCol>)
+                    }
                 })}
             </IonRow>
         </IonGrid>
