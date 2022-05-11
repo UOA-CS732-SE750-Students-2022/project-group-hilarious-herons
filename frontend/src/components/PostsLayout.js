096aa2bb4800c904a3b95c1097f224e4d3f154e8
KirstyGong <kirstygong23@gmail.com>
2022-05-11 18:55:45 +1200

    fix post positioning

--- frontend/src/components/PostsLayout.js
+++ frontend/src/components/PostsLayout.js
@@ -13,7 +13,7 @@ export const PostsLayout = ({ dataForCards = [] }) => {
                 // sizeLg="auto"
                 key={index}
                 class="no-padding text-center ion-item "
-                style={{ width: "365px" }}
+                style={{ maxWidth: "365px" }}
               >
                 <FoodCard
                   id={cardData.id}
