b79f83ba48b9ddc55752f225decd102c338b388b
Yujia Wu <ywu660@aucklanduni.ac.nz>
2022-05-11 17:38:56 +0800

    Style the code

--- frontend/src/components/PostsLayout.js
+++ frontend/src/components/PostsLayout.js
@@ -2,20 +2,31 @@ import { IonGrid, IonRow, IonCol, IonContent, IonText } from '@ionic/react';
 import FoodCard from './FoodCard';
 
 export const PostsLayout = ({ dataForCards = [], isNoSearchResult }) => {
-    console.log(isNoSearchResult);
     if(!isNoSearchResult) {
         return (
-                <IonGrid>
+            <IonGrid>
                 <IonRow class="ion-justify-content-center no-padding">
-                    {dataForCards.map((cardData, index) => {
+                    { dataForCards.map((cardData, index) => {
                         if (typeof cardData.image !== 'undefined') {
                             return (
-                                <IonCol size='auto' key={index} class="no-padding text-center ion-item " style={{ maxWidth: "365px", minWidth: "365px" }}>
-                                    <FoodCard id={cardData.id} image={cardData.image} foodName={cardData.foodName} rating={cardData.rating} timestamp={cardData.timestamp} numberOfLikes={cardData.numberOfLikes} postLiked={cardData.postLiked} />
+                                <IonCol 
+                                    size='auto' 
+                                    key={index} 
+                                    class="no-padding text-center ion-item " 
+                                    style={{ maxWidth: "365px", minWidth: "365px" }}
+                                >
+                                    <FoodCard 
+                                        id={cardData.id} 
+                                        image={cardData.image} 
+                                        foodName={cardData.foodName} 
+                                        rating={cardData.rating} 
+                                        timestamp={cardData.timestamp} 
+                                        umberOfLikes={cardData.numberOfLikes} 
+                                        postLiked={cardData.postLiked} />
                                 </IonCol>
                         );
                         } else {
-                        return (<IonCol key={index}></IonCol>)
+                            return (<IonCol key={index}></IonCol>)
                         }
                     })}
                 </IonRow>
