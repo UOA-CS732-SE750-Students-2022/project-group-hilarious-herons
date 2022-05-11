df60db3d79ecd38db72bc9d8c942a5515899b31f
KirstyGong <kirstygong23@gmail.com>
2022-05-11 16:09:12 +1200

    Added loading for get Post

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,20 +2,20 @@ import { FoodPage } from "../components/FoodPage";
 import { postDataForFoodCard } from "../utils/postManager";
 import { PostsLayout } from "../components/PostsLayout";
 import { useEffect, useState } from "react";
+import { Loading } from "../components/Loading";
 
 export const HomePage = () => {
-
   const [data, updateData] = useState();
   useEffect(() => {
     const getData = async () => {
       const resp = await postDataForFoodCard();
       updateData(resp);
-    }
+    };
     getData();
   }, []);
   return (
     <FoodPage banner>
-      <PostsLayout dataForCards={data} />
+      {data ? <PostsLayout dataForCards={data} /> : <Loading />}
     </FoodPage>
   );
 };
