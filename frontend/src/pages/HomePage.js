ac1f0b0ed98cfede2ca32ad23f8d4670873cd95d
alyu201 <alyu201@aucklanduni.ac.nz>
2022-05-12 02:34:44 +1200

    Add post button for signed in users only

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,20 +2,21 @@ import { FoodPage } from "../components/FoodPage";
 import { postDataForFoodCard } from "../utils/postManager";
 import { PostsLayout } from "../components/PostsLayout";
 import { useEffect, useState } from "react";
+import { AddPostButton } from "../components/AddPostButton";
 
 export const HomePage = () => {
-
   const [data, updateData] = useState();
   useEffect(() => {
     const getData = async () => {
       const resp = await postDataForFoodCard();
       updateData(resp);
-    }
+    };
     getData();
   }, []);
   return (
     <FoodPage banner>
       <PostsLayout dataForCards={data} />
+      <AddPostButton />
     </FoodPage>
   );
 };
