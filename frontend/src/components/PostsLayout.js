a07b6afe8c268c249bba1e6de10716e1bf11aff3
henrym25 <henry3399@hotmail.co.nz>
2022-05-13 18:08:09 +1200

    Added sort popover

--- frontend/src/components/PostsLayout.js
+++ frontend/src/components/PostsLayout.js
@@ -1,18 +1,97 @@
-import { IonGrid, IonRow, IonCol, IonText } from "@ionic/react";
+import {
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonIcon,
+  IonFabButton,
+  IonPopover,
+  IonRadioGroup,
+  IonLabel,
+  IonItem,
+  IonRadio,
+  IonListHeader,
+  IonList,
+} from "@ionic/react";
 import FoodCard from "./FoodCard";
 import { BackHomeButton } from "./BackHomeButton";
+import { useEffect, useState } from "react";
+import { funnel, arrowUp, arrowDown } from "ionicons/icons";
 
 export const PostsLayout = ({
   isHomePage = false,
   dataForCards = [],
   isNoSearchResult,
 }) => {
+  const unsorted = dataForCards;
+
+  const [cardsToDisplay, setCardsToDisplay] = useState(unsorted);
+  const [sortOrder, setSortOrder] = useState("ASC");
+  const [showMobileModal, setShowMobileModal] = useState(false);
+
+  useEffect(() => {
+    let sorted = [...unsorted];
+    switch (sortOrder) {
+      case "ASC":
+        sorted.sort((a, b) => (a.rating > b.rating ? 1 : -1));
+        break;
+      case "DESC":
+        sorted.sort((a, b) => (a.rating < b.rating ? 1 : -1));
+        break;
+      default:
+        sorted = [...unsorted];
+        break;
+    }
+    setCardsToDisplay(sorted);
+  }, [sortOrder]);
+
   if (!isNoSearchResult) {
     return (
       <IonGrid>
         {isHomePage ? <></> : <BackHomeButton />}
+        <IonRow class="ion-justify-content-end ion-padding-end">
+          <IonFabButton
+            size="large"
+            color="light"
+            className="ion-no-shadows"
+            id="sort-button"
+          >
+            <IonIcon icon={funnel} size="small" />
+          </IonFabButton>
+          <IonPopover trigger="sort-button" size="auto" mode="ios">
+            <IonList>
+              <IonRadioGroup onIonChange={(e) => setSortOrder(e.target.value)}>
+                <IonListHeader>
+                  <IonLabel>Select Sort order</IonLabel>
+                </IonListHeader>
+
+                <IonItem>
+                  <IonLabel>
+                    Rating <IonIcon icon={arrowUp} size="small" />
+                  </IonLabel>
+                  <IonRadio value="DESC" />
+                </IonItem>
+
+                <IonItem>
+                  <IonLabel>
+                    Rating <IonIcon icon={arrowDown} size="small" />
+                  </IonLabel>
+                  <IonRadio value="ASC" />
+                </IonItem>
+
+                <IonItem>
+                  <IonLabel>
+                    Distance <IonIcon icon={arrowDown} size="small" />
+                  </IonLabel>
+                  <IonRadio value="" />
+                </IonItem>
+              </IonRadioGroup>
+            </IonList>
+          </IonPopover>
+        </IonRow>
+
         <IonRow class="ion-justify-content-center no-padding">
-          {dataForCards.map((cardData, index) => {
+          {cardsToDisplay.map((cardData, index) => {
             if (typeof cardData.image !== "undefined") {
               return (
                 <IonCol
