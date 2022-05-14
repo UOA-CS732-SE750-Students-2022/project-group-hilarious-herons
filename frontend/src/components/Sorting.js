a67f6e57c070165ee66e27d8b4b395ef20b1c552
henrym25 <henry3399@hotmail.co.nz>
2022-05-15 04:07:29 +1200

    Sorting now stays ticked

--- frontend/src/components/Sorting.js
+++ frontend/src/components/Sorting.js
@@ -1,55 +1,54 @@
 import React from "react";
 import {
-    IonRow,
-    IonIcon,
-    IonFabButton,
-    IonPopover,
-    IonRadioGroup,
-    IonLabel,
-    IonItem,
-    IonRadio,
-    IonList,
+  IonRow,
+  IonIcon,
+  IonFabButton,
+  IonPopover,
+  IonRadioGroup,
+  IonLabel,
+  IonItem,
+  IonRadio,
+  IonList,
 } from "@ionic/react";
 import { funnel, arrowUp, arrowDown } from "ionicons/icons";
 
-export const Sorting = ({
-  setSortOrder 
-}) => {
-    return(
-        <IonRow class="ion-justify-content-end ion-padding-end">
-        <IonFabButton
-          size="large"
-          color="light"
-          className="ion-no-shadows"
-          id="sort-button"
-          mode="ios"
-        >
-          <IonIcon icon={funnel} size="small" title="sorting"/>
-        </IonFabButton>
-        <IonPopover trigger="sort-button" size="auto" mode="ios">
-          <IonList lines="none">
-            <IonRadioGroup 
-              allowEmptySelection 
-              onIonChange={(e) => setSortOrder(e.target.value)}
-            >
-              <IonItem>
-                <IonLabel>
-                  Rating 
-                  <IonIcon icon={arrowUp} size="small" />
-                </IonLabel>
-                <IonRadio value="RATING DESC" />
-              </IonItem>
+export const Sorting = ({ setSortOrder, sortOrder }) => {
+  return (
+    <IonRow class="ion-justify-content-end ion-padding-end">
+      <IonFabButton
+        size="large"
+        color="light"
+        className="ion-no-shadows"
+        id="sort-button"
+        mode="ios"
+      >
+        <IonIcon icon={funnel} size="small" title="sorting" />
+      </IonFabButton>
+      <IonPopover trigger="sort-button" size="auto" mode="ios">
+        <IonList lines="none">
+          <IonRadioGroup
+            allowEmptySelection
+            onIonChange={(e) => setSortOrder(e.target.value)}
+            value={sortOrder}
+          >
+            <IonItem>
+              <IonLabel>
+                Rating
+                <IonIcon icon={arrowUp} size="small" />
+              </IonLabel>
+              <IonRadio value="RATING DESC" />
+            </IonItem>
 
-              <IonItem>
-                <IonLabel>
-                  Rating 
-                  <IonIcon icon={arrowDown} size="small" />
-                </IonLabel>
-                <IonRadio value="RATING ASC" />
-              </IonItem>
-            </IonRadioGroup>
-          </IonList>
-        </IonPopover>
-      </IonRow>
-  )
-}
+            <IonItem>
+              <IonLabel>
+                Rating
+                <IonIcon icon={arrowDown} size="small" />
+              </IonLabel>
+              <IonRadio value="RATING ASC" />
+            </IonItem>
+          </IonRadioGroup>
+        </IonList>
+      </IonPopover>
+    </IonRow>
+  );
+};
