c74a4dc1cdeb3cb7724e7d8280962ba74f81d62f
alyu201 <alyu201@aucklanduni.ac.nz>
2022-05-14 16:28:11 +1200

    Add no posts found message

--- frontend/src/components/PostsLayout.js
+++ frontend/src/components/PostsLayout.js
@@ -13,36 +13,59 @@ export const PostsLayout = ({
   isNoSearchResult = false,
 }) => {
   if (!isNoSearchResult) {
-    return (
-      <IonGrid>
-        <IonRow class="ion-justify-content-center no-padding">
-          {dataForCards.map((cardData, index) => {
-            if (typeof cardData.image !== "undefined") {
-              return (
-                <IonCol
-                  size="auto"
-                  key={index}
-                  class="no-padding text-center ion-item "
-                  style={{ maxWidth: "365px", minWidth: "365px" }}
-                >
-                  <FoodCard
-                    id={cardData.id}
-                    image={cardData.image}
-                    foodName={cardData.foodName}
-                    rating={cardData.rating}
-                    timestamp={cardData.timestamp}
-                    numberOfLikes={cardData.numberOfLikes}
-                    postLiked={cardData.postLiked}
-                  />
-                </IonCol>
-              );
-            } else {
-              return <IonCol key={index}></IonCol>;
-            }
-          })}
-        </IonRow>
-      </IonGrid>
-    );
+    if (dataForCards.length === 0) {
+      return (
+        <IonGrid style={{ textAlign: "center", margin: "1.5rem" }}>
+          <img
+            src="/no-results-img.png"
+            alt="Natasha Remarchuk"
+            className="img"
+          />
+          <p className="img-attribution">
+            Illustration by{" "}
+            <a href="https://icons8.com/illustrations/author/292791">
+              Anna Golde
+            </a>{" "}
+            from <a href="https://icons8.com/illustrations">Ouch!</a>
+          </p>
+
+          <IonText color="ionContentHeaderText" className="title-msg">
+            <h1>No posts found</h1>
+          </IonText>
+        </IonGrid>
+      );
+    } else {
+      return (
+        <IonGrid>
+          <IonRow class="ion-justify-content-center no-padding">
+            {dataForCards.map((cardData, index) => {
+              if (typeof cardData.image !== "undefined") {
+                return (
+                  <IonCol
+                    size="auto"
+                    key={index}
+                    class="no-padding text-center ion-item "
+                    style={{ maxWidth: "365px", minWidth: "365px" }}
+                  >
+                    <FoodCard
+                      id={cardData.id}
+                      image={cardData.image}
+                      foodName={cardData.foodName}
+                      rating={cardData.rating}
+                      timestamp={cardData.timestamp}
+                      numberOfLikes={cardData.numberOfLikes}
+                      postLiked={cardData.postLiked}
+                    />
+                  </IonCol>
+                );
+              } else {
+                return <IonCol key={index}></IonCol>;
+              }
+            })}
+          </IonRow>
+        </IonGrid>
+      );
+    }
   } else {
     return (
       <IonGrid style={{ textAlign: "center", margin: "1.5rem" }}>
--- frontend/src/pages/AccountPage.js
+++ frontend/src/pages/AccountPage.js
@@ -70,6 +70,7 @@ export const AccountPage = () => {
     }
   };
 
+  // Fetch posts on load
   useEffect(() => {
     fetchPosts();
   }, []);
